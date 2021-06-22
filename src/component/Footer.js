292aeaa42d5b2e467a52356b16dbe532252bc0bb
DongMZ1 <915094594@qq.com>
2021-06-22 01:51:03 -0400

    a creative design in my head, update the app

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -2,12 +2,14 @@ import React from 'react'
 import {MdCall} from 'react-icons/md'
 import {BsFillPersonLinesFill} from 'react-icons/bs'
 import {AiFillSetting} from 'react-icons/ai'
-import {GrApps} from 'react-icons/gr'
+import {SiAppstore} from 'react-icons/si'
+import {RiAppsFill} from 'react-icons/ri'
 const Footer = ({numberofunread}) =>{
     return <div className='footer'>
           <div style={{position:'relative'}}><MdCall /><span className='callicon'>{numberofunread}</span></div>
           <div style={{color:'grey'}}><BsFillPersonLinesFill /></div>
-          <div style={{color:'grey'}}><GrApps /></div>
+          <div className='centericon'><RiAppsFill style={{color:'white'}} /></div>
+          <div style={{color:'grey'}}><SiAppstore /></div>
           <div style={{color:'grey'}}><AiFillSetting /></div>
     </div>
 }
